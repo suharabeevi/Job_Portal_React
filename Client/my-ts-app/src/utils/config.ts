4cbc07f6dad51366086a3967eed1603229fa6abd
suhara beevi <sairasuhara12345@gmail.com>
2024-05-18 10:50:05 +0530

    .

--- Client/my-ts-app/src/utils/config.ts
+++ Client/my-ts-app/src/utils/config.ts
@@ -1,8 +1,8 @@
 const configKeys = {
-    API_URL: "http://localhost:8080/api/",
-    SOCKET_PORT: "http://localhost:8080",
-    // API_URL: "https://hirehub.site/api/",
-    // SOCKET_PORT: "https://hirehub.site",
+    // API_URL: "http://localhost:8080/api/",
+    // SOCKET_PORT: "http://localhost:8080",
+     API_URL: "https://job-portal-react.onrender.com/api/",
+     SOCKET_PORT: "https://job-portal-react.onrender.com",
   };
    
   export default configKeys;
