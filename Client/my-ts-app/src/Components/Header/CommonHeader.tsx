fe550beba946c19262577d1ac8e11a4e9d09f181
suhara beevi <sairasuhara12345@gmail.com>
2024-05-18 10:39:50 +0530

    bug fix

--- Client/my-ts-app/src/Components/Header/CommonHeader.tsx
+++ Client/my-ts-app/src/Components/Header/CommonHeader.tsx
@@ -22,7 +22,7 @@ const navigation = [
               <div className="relative flex h-16 items-center justify-between">
                 <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                   {/* Mobile menu button*/}
-                  <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <Disclosure.Button className="inline-flex items-center justify-center  rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                     <span className="sr-only">Open main menu</span>
                     {open ? (
                       <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
@@ -63,7 +63,7 @@ const navigation = [
                         href={item.href}
                         className={classNames(
                           item.current
-                            ? "bg-brown-600 text-white"
+                            ? "bg-brown-600 text-white hover:bg-opacity-70"
                             : "text-black hover:bg-brown-500 hover:text-white",
                           "rounded-md px-3 py-2 text-sm font-medium"
                         )}
@@ -76,7 +76,6 @@ const navigation = [
                 </div>
               </div>
             </div>
-  
             <Disclosure.Panel className="sm:hidden">
               <div className="space-y-1 px-2 pb-3 pt-2">
                 {navigation.map((item) => (
@@ -86,7 +85,7 @@ const navigation = [
                     href={item.href}
                     className={classNames(
                       item.current
-                        ? "bg-gray-900 text-white"
+                        ? "bg-gray-900 text-white "
                         : "text-gray-300 hover:bg-gray-700 hover:text-white",
                       "block rounded-md px-3 py-2 text-base font-medium"
                     )}
--- Client/my-ts-app/src/utils/config.ts
+++ Client/my-ts-app/src/utils/config.ts
@@ -1,6 +1,6 @@
 const configKeys = {
     API_URL: "http://localhost:8080/api/",
-    SOCKET_PORT: "http://localhost:3000",
+    SOCKET_PORT: "http://localhost:8080",
     // API_URL: "https://hirehub.site/api/",
     // SOCKET_PORT: "https://hirehub.site",
   };
